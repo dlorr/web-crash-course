93f128d8751d3999958e3415fb9c22bdf2b9e398
dlorr <hyesrold@gmail.com>
2026-03-23 20:32:55 +0800

    course - vuejs

---
+++ 4-vue/vite/src/AppComposition.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const message = ref("Hello World! Data");
+const status = ref("pending");
+const tasks = ref(["Task 1", "Task 2", "Task 3"]);
+const link = ref("https://google.com");
+const newTask = ref("");
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+
+const addTask = (e) => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.log(error);
+  }
+});
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <p v-if="status === 'active'">I am active</p>
+  <p v-else-if="status === 'pending'">I am pending</p>
+  <p v-else>I am inactive</p>
+
+  <form @submit="addTask">
+    <label for="newTask">Add Task</label>
+    <br />
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <br />
+    <button type="submit">Submit</button>
+  </form>
+
+  <h1>Tasks</h1>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
+  <!-- <a v-bind:href="link">Google</a> -->
+  <a :href="link">Google</a>
+  <br />
+  <button @click="toggleStatus">Change status</button>
+</template>
--- 4-vue/vite/vite.config.js
+++ 4-vue/vite/vite.config.js
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -14,5 +14,12 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
